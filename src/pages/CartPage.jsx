376d1ed54f8a1fb7d80662770049a06422fb879c
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-07-19 17:59:05 +1000

    feat: update some icon for cart

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,26 @@
 import "../styles/pages/CartPage.css"
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+
 
 export default function CartPage(){
 
 
 
     return(
+        <div id="cart-container">
+            
+             <h1>This is cart page!!!</h1>
+            <label>This is cart icon</label>
+            <div id="icon">
+                <AddShoppingCartIcon  />
+                <RemoveCircleOutlineIcon />
+                <AddCircleOutlineIcon />
+            </div>
+            
 
-        <h1>This is cart page!!!</h1>
+        </div>
     )
 }
