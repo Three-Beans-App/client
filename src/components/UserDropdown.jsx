945de41b00f0806c88048a1159ba18252ba37c23
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-07-26 14:37:48 +1000

    fix the delete unuse item

--- src/components/UserDropdown.jsx
+++ src/components/UserDropdown.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState,  useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import "../styles/components/UserDropdown.css";
