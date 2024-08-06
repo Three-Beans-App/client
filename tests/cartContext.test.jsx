0eae2de3c8d68e5faaad8c4badebd34026db8510
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-08-06 22:42:51 +1000

    test: test for handleQuantityChange

--- tests/cartContext.test.jsx
+++ tests/cartContext.test.jsx
@@ -12,53 +12,82 @@ describe('Cart Context', () => {
         wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
     });
 
-    it('should add an item to the cart', () => {
-        const { result } = renderHook(() => {
-            const data = useCartData();
-            const dispatch = useCartDispatch();
-            return { data, dispatch };
-        }, { wrapper });
 
-        act(() => {
-            result.current.dispatch.handleAddToCart({ 
-                name: 'Item 1',
-                price: 10
+    describe('handleAddToCart', () => {
+        it('should add an item to the cart', () => {
+            const { result } = renderHook(() => {
+                const data = useCartData();
+                const dispatch = useCartDispatch();
+                return { data, dispatch };
+            }, { wrapper });
+    
+            act(() => {
+                result.current.dispatch.handleAddToCart({ 
+                    name: 'Test Item',
+                    price: 10
+                });
             });
+    
+            expect(result.current.data.cartItems.length).toBe(1);
+            expect(result.current.data.cartItems[0].item.name).toBe('Test Item');
+            expect(result.current.data.cartItems[0].quantity).toBe(1);
         });
-
-        expect(result.current.data.cartItems.length).toBe(1);
-        expect(result.current.data.cartItems[0].item.name).toBe('Item 1');
-        expect(result.current.data.cartItems[0].quantity).toBe(1);
+    
+        it('should update the quantity of an item in the cart', () => {
+            const { result: initialResult } = renderHook(() => {
+                const data = useCartData();
+                const dispatch = useCartDispatch();
+                return { data, dispatch };
+            }, { wrapper });
+    
+            act(() => {
+                initialResult.current.dispatch.handleAddToCart({
+                    name: 'Test Item',
+                    price: 10
+                });
+            });
+    
+            const { result: updatedResult } = renderHook(() => {
+                const data = useCartData();
+                const dispatch = useCartDispatch();
+                return { data, dispatch}
+            }, { wrapper });
+    
+            act(() => {
+                updatedResult.current.dispatch.handleAddToCart({
+                    name: 'Test Item',
+                    price: 10
+                });
+            });
+    
+            expect(updatedResult.current.data.cartItems.length).toBe(1);
+            expect(updatedResult.current.data.cartItems[0].quantity).toBe(2);     
+        });
     });
+    
+    
+    describe('handleQuantityChange', () => {
+        it('should change the quantity of an item in the cart', () => {
+            const { result: initialResult } = renderHook(() => {
+                const data = useCartData();
+                const dispatch = useCartDispatch();
+                return { data, dispatch };
+            }, { wrapper });
 
-    it('should update the quantity of an item in the cart', () => {
-        const { result: initialResult } = renderHook(() => {
-            const data = useCartData();
-            const dispatch = useCartDispatch();
-            return { data, dispatch };
-        }, { wrapper });
-
-        act(() => {
-            initialResult.current.dispatch.handleAddToCart({
-                name: 'Item 1',
-                price: 10
+            act(() => {
+                initialResult.current.dispatch.handleAddToCart({
+                    name: 'Test Item',
+                    price: 10
+                });
             });
-        });
 
-        const { result: updatedResult } = renderHook(() => {
-            const data = useCartData();
-            const dispatch = useCartDispatch();
-            return { data, dispatch}
-        }, { wrapper });
-
-        act(() => {
-            updatedResult.current.dispatch.handleAddToCart({
-                name: 'Item 1',
-                price: 10
+            act(() => {
+                initialResult.current.dispatch.handleQuantityChange('Test Item', 5);
             });
-        });
 
-        expect(updatedResult.current.data.cartItems.length).toBe(1);
-        expect(updatedResult.current.data.cartItems[0].quantity).toBe(2);     
+            const { result: updatedResult } = renderHook(() => useCartData(), { wrapper });
+
+            expect(updatedResult.current.cartItems[0].quantity).toBe(5);
+        });
     });
 });
