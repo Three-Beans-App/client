6e9f0399288cdcac71b7f89b73e6b8382728111f
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-08-06 22:32:15 +1000

    test: update quantity for handleAddToCart

--- tests/cartContext.test.jsx
+++ tests/cartContext.test.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { renderHook } from "@testing-library/react";
 import { act } from "react";
-import CartProvider, { useCartData, useCartDispatch, UseCartDispatch } from "../src/contexts/cartContext";
+import CartProvider, { useCartData, useCartDispatch } from "../src/contexts/cartContext";
+import { useActionData } from "react-router-dom";
 
 describe('Cart Context', () => {
+    let wrapper;
+
     beforeEach(() => {
         localStorage.clear();
-    }) ;
+        wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
+    });
 
     it('should add an item to the cart', () => {
-        const wrapper = ({ children }) => <CartProvider>{children}</CartProvider>;
-
         const { result } = renderHook(() => {
             const data = useCartData();
             const dispatch = useCartDispatch();
@@ -27,5 +29,36 @@ describe('Cart Context', () => {
         expect(result.current.data.cartItems.length).toBe(1);
         expect(result.current.data.cartItems[0].item.name).toBe('Item 1');
         expect(result.current.data.cartItems[0].quantity).toBe(1);
+    });
+
+    it('should update the quantity of an item in the cart', () => {
+        const { result: initialResult } = renderHook(() => {
+            const data = useCartData();
+            const dispatch = useCartDispatch();
+            return { data, dispatch };
+        }, { wrapper });
+
+        act(() => {
+            initialResult.current.dispatch.handleAddToCart({
+                name: 'Item 1',
+                price: 10
+            });
+        });
+
+        const { result: updatedResult } = renderHook(() => {
+            const data = useCartData();
+            const dispatch = useCartDispatch();
+            return { data, dispatch}
+        }, { wrapper });
+
+        act(() => {
+            updatedResult.current.dispatch.handleAddToCart({
+                name: 'Item 1',
+                price: 10
+            });
+        });
+
+        expect(updatedResult.current.data.cartItems.length).toBe(1);
+        expect(updatedResult.current.data.cartItems[0].quantity).toBe(2);     
     });
 });
