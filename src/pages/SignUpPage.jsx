c63cdc3d7b3f2d703e19b715d9a7ab5d0874a6e2
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-08-09 11:09:43 +1000

    polish

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -35,36 +35,36 @@ export default function SignUpPage(){
                         </div>
                     </div>
                 ) : (
-                    <form onSubmit={handleSignup}>
-                    <label>Your Name:</label>
-                    <input 
-                        type="text" name="name" id="name"
-                        value={name} onChange={(event)=> setName(event.target.value)}
+                    <form onSubmit={handleSignup} >
+                        <label>Your Name:</label>
+                        <input 
+                            type="text" name="name" id="name"
+                            value={name} onChange={(event)=> setName(event.target.value)}
+                            />
+                        <label>Your Birthday:</label>
+                        <input 
+                            type="date" name="birthday" id="birthday" 
+                            value={birthday} onChange={(event)=> setBirthday(event.target.value)}
+                            />
+                        <label>Sign Up email: </label>
+                        <input 
+                            type="text" name="email" id="email"
+                            value={email} onChange={(event)=> setEmail(event.target.value)}
+                            />
+                        <label>Sign Up Password: </label>
+                        <input 
+                            type="password" name="pssword" id="password"
+                            value={password} onChange={(event)=>setPassword(event.target.value)}
                         />
-                    <label>Your Birthday:</label>
-                    <input 
-                        type="date" name="birthday" id="birthday" 
-                        value={birthday} onChange={(event)=> setBirthday(event.target.value)}
-                        />
-                    <label>Sign Up email: </label>
-                    <input 
-                        type="text" name="email" id="email"
-                        value={email} onChange={(event)=> setEmail(event.target.value)}
-                        />
-                    <label>Sign Up Password: </label>
-                    <input 
-                        type="password" name="pssword" id="password"
-                        value={password} onChange={(event)=>setPassword(event.target.value)}
-                    />
-                    <div id="spacer" />
-                    <button type="submit" >
-                        Sign Up
-                    </button>
-                    {error && 
-                        <div className="error">{error}</div>
-                    }
+                        <div id="spacer" />
+                        <button type="submit" >
+                            Sign Up
+                        </button>
+                        {error && 
+                            <div className="error">{error}</div>
+                        }
                     
-                </form>
+                     </form>
 
 
             )}      
